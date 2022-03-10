a5a3079cb89297cf03da473b801fd4872396c896
ryanchang168 <ryanchang.cs06@nctu.edu.tw>
2022-03-10 15:52:53 +0800

    Finally finished Rpi part

--- Lab1/src/main.c
+++ Lab1/src/main.c
@@ -6,7 +6,7 @@
 void main(){
 	UART_init();
 	//UART_read();
-	UART_put("\r\ntest\r\n");
+	//UART_put("\r\ntest\r\n");
 
 	Board_revision();
 	UART_put("Board revision : ");
@@ -21,9 +21,12 @@ void main(){
 	UART_hex(mailbox[6]);
 	UART_put("\r\n");
 
-	char str[10] = "";
+	char str[10];
+	for(int i=0;i<10;i++)
+		str[i] = 0;
 	int idx = 0;
-
+	
+	//UART_put("# ");
 	while(1){
 		char c = UART_read();
 		UART_write(c);
@@ -31,12 +34,12 @@ void main(){
 		if(c != '\n')
 			str[idx++] = c;
 		else{
-			if(!Strncmp(str, "help", 4))
-				UART_put("help      : print this help menu\nhello     : print Hello World!\nreboot    : reboot the device\r\n");
-			else if(!Strncmp(str, "hello", 5))
-				UART_put("Hello World!\r\n");
-			else if(!Strncmp(str, "reboot", 6)){
-				//UART_put("\n");
+			if(Strncmp(str, "help", 4)==0)
+				UART_put("\rhelp      : print this help menu\nhello     : print Hello World!\nreboot    : reboot the device\r\n# ");
+			else if(Strncmp(str, "hello", 5)==0)
+				UART_put("\rHello World!\r\n# ");
+			else if(Strncmp(str, "reboot", 6)==0){
+				UART_put("\r");
 				reset(1);
 			}
 			else
